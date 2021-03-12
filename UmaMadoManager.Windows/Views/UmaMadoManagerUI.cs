b7aba059ee3e94507d378553f08d0d16ef3a1e96
yamachu <yamachu.dev@gmail.com>
2021-03-13 06:28:22 +0900

    UIに反映

--- UmaMadoManager.Windows/Views/UmaMadoManagerUI.cs
+++ UmaMadoManager.Windows/Views/UmaMadoManagerUI.cs
@@ -98,9 +98,46 @@ namespace UmaMadoManager.Windows.Views
                 }),
             });
 
+            var muteSettingMenus = new ToolStripMenuItem()
+            {
+                Text = "Mute",
+            };
+            muteSettingMenus.DropDownItems.AddRange(new ToolStripItem[]{
+                new ToolStripMenuItem("Nop").Also(v => {
+                    this.Disposable.Add(Observable.FromEventPattern(v, nameof(v.Click)).Subscribe(x => {
+                        _VM.MuteCondition.Value = MuteCondition.Nop;
+                    }));
+                    this.Disposable.Add(_VM.MuteCondition.Subscribe(x => {
+                        v.Checked = x == MuteCondition.Nop;
+                    }));
+                    v.CheckOnClick = true;
+                }),
+                new ToolStripMenuItem("When Background").Also(v => {
+                    this.Disposable.Add(Observable.FromEventPattern(v, nameof(v.Click)).Subscribe(x => {
+                        _VM.MuteCondition.Value = MuteCondition.WhenBackground;
+                    }));
+                    this.Disposable.Add(_VM.MuteCondition.Subscribe(x => {
+                        v.Checked = x == MuteCondition.WhenBackground;
+                    }));
+                    v.CheckOnClick = true;
+                }),
+                new ToolStripMenuItem("When Minimized").Also(v => {
+                    this.Disposable.Add(Observable.FromEventPattern(v, nameof(v.Click)).Subscribe(x => {
+                        _VM.MuteCondition.Value = MuteCondition.WhenMinimize;
+                    }));
+                    this.Disposable.Add(_VM.MuteCondition.Subscribe(x => {
+                        v.Checked = x == MuteCondition.WhenMinimize;
+                    }));
+                    v.CheckOnClick = true;
+                }),
+            });
+
             contextMenu.Items.AddRange(new ToolStripItem[]{
                 verticalSettingMenus,
                 horizontalSettingMenus,
+                new ToolStripSeparator(),
+                muteSettingMenus,
+                new ToolStripSeparator(),
                 new ToolStripMenuItem("Exit").Also(v => {
                     v.Click += (_, _) => {
                         trayNotifyIcon.Icon = null;
