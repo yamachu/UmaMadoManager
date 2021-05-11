33392bba7a46560f49fb2bf7d8b70919d0cf77fa
yamachu <yamachu.dev@gmail.com>
2021-05-12 01:06:08 +0900

    WPFのViewでEnumをBindingしたいのでConverter実装しておく

---
+++ UmaMadoManager.Windows/Converters/MuteConditionEnumConveter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using UmaMadoManager.Core.Models;
+
+namespace UmaMadoManager.Windows.Converters
+{
+    class MuteConditionEnumConveter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var targetEnum = (MuteCondition)parameter;
+            var passedEnum = (MuteCondition)value;
+
+            return targetEnum == passedEnum;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return parameter;
+        }
+    }
+}
