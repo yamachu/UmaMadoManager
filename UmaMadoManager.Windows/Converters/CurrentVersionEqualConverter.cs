e8a6c3391847c302cd232788517936c73d33d9ad
yamachu <yamachu.dev@gmail.com>
2021-05-12 02:01:47 +0900

    現在のバージョンの比較のためのConverterの実装

---
+++ UmaMadoManager.Windows/Converters/CurrentVersionEqualConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Windows.Data;
+using UmaMadoManager.Core.Extensions;
+
+namespace UmaMadoManager.Windows.Converters
+{
+    class CurrentVersionEqualConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value as string == "")
+            {
+                return System.Windows.Visibility.Collapsed;
+            }
+            var currentVersionWithoutRevision = Assembly.GetExecutingAssembly().GetName().Version.Let(currentVersion => {
+                return new Version(currentVersion.Major, currentVersion.Minor, currentVersion.Build);
+            });
+            var formattedVersion = Version.Parse(value as string);
+            return currentVersionWithoutRevision != formattedVersion ? System.Windows.Visibility.Visible : System.Windows.Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
