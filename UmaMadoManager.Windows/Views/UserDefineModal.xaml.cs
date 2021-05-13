ed084d73deac785ac0c2ae60826d128bc630c8d5
yamachu <yamachu.dev@gmail.com>
2021-05-14 01:42:56 +0900

    User指定位置のモーダルの実装

---
+++ UmaMadoManager.Windows/Views/UserDefineModal.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+
+namespace UmaMadoManager.Windows.Views
+{
+    /// <summary>
+    /// UserDefineModal.xaml の相互作用ロジック
+    /// </summary>
+    public partial class UserDefineModal : Window
+    {
+        public Action OnClickUsingPrevious { get;  set; }
+        public Action OnClickUsingCurrent { get; set; }
+
+
+        public UserDefineModal()
+        {
+            InitializeComponent();
+        }
+
+        public UserDefineModal(bool enablePrevious)
+        {
+            InitializeComponent();
+            this.previousButton.IsEnabled = enablePrevious;
+        }
+
+        private void _onClickUsingPrevious(object sender, RoutedEventArgs e)
+        {
+            this.OnClickUsingPrevious?.Invoke();
+        }
+
+        private void _onClickUsingCurrent(object sender, RoutedEventArgs e)
+        {
+            this.OnClickUsingCurrent?.Invoke();
+        }
+    }
+}
